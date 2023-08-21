fc79fca4d57d34951d5ddda30c83ebe39dba703c
Blair Wong <blairwwong@gmail.com>
2023-08-21 11:46:17 -0400

    Fixing marker

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
         integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
         crossOrigin=""
       />
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/Leaflet.awesome-markers/2.0.2/leaflet.awesome-markers.min.js" integrity="sha512-8BqQ2RH4L4sQhV41ZB24fUc1nGcjmrTA6DILV/aTPYuUzo+wBdYdp0fvQ76Sxgf36p787CXF7TktWlcxu/zyOg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
       <NavigationBar></NavigationBar>
       <Map></Map>
     </div>
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,26 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import './Map.css';
 
 function Map() {
-    const position = [42.3876, 71.0995]
+    const position = [42.4, -71.1]
+
+    let DefaultIcon = L.icon({
+        iconUrl: icon,
+        shadowUrl: iconShadow
+    });
+
+    L.Marker.prototype.options.icon = DefaultIcon;
 
     return (
-        <MapContainer center={[42.4, -71.1]} zoom={13} scrollWheelZoom={false} className="leaflet-container">
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
             <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
+            />
             <Marker position={position}>
             <Popup>
                 A pretty CSS3 popup. <br /> Easily customizable.
